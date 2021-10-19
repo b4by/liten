d768533e1eac79ed4c13f6e456a6bf3209a26fd1
Leonid <asal9000@gmail.com>
2021-10-19 13:28:58 +0300

    changed logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/svg/logo.svg";
+import logo from "../../assets/svg/szb-logo-white.svg";
 
 export const Logo = ({ className }) => {
   return <img src={logo} className={className} alt="logo" />;
