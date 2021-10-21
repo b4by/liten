f9e6855cba984571593bdc3a9caab4b1b33108e9
Daniil Potapov <Daniil.Potapov@mirapolis.ru>
2021-10-21 03:00:11 +0300

    holy shit

--- src/pages/task/task_4/index.jsx
+++ src/pages/task/task_4/index.jsx
@@ -46,8 +46,8 @@ import {
   Container,
   Wrapper2,
 } from "./styled";
-import * as Tone from "tone";
-import { useEffect, useRef, useState, useCallback, forwardRef } from "react";
+import Tone from "tone";
+import { useEffect, useRef, useState, useCallback, memo } from "react";
 import playIconImg from "../../../assets/img/play-icon@2x.png";
 import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 import Draggable from "react-draggable";
@@ -59,41 +59,34 @@ function freqFromX(coordX, width) {
   return (coordX / width) * 250 + 250;
 }
 
-const DraggableBox = forwardRef(({ id, item: Item }, ref) => {
-  const updateXarrow = useXarrow();
+const DraggableBox = memo(({ id, item: Item }) => {
+  // const updateXarrow = useXarrow();
+  console.log("@@@@RERENDER");
   return (
-    <Draggable onDrag={throttle(updateXarrow, 100)} onStop={updateXarrow}>
+    <Draggable>
       <Item
         id={id}
-        style={{ position: "absolute", zIndex: "1000", opacity: 0.01 }}
+        style={{ position: "relative", zIndex: "1000", opacity: 1 }}
       />
     </Draggable>
   );
 });
 
 const ToneButton = ({ children, note }) => {
-  return <div data-note={note}>{children}</div>;
+  return (
+    <div draggable={false} data-note={note}>
+      {children}
+    </div>
+  );
 };
 
-let synth = new Tone.MonoSynth({
-  harmonicity: 2,
-  vibratoAmount: 0.1,
-  voice0: {
-    oscillator: {
-      type: "sine",
-    },
-  },
-  voice1: {
-    oscillator: {
-      type: "sine",
-    },
-  },
-}).toDestination();
+const synth = new Tone.FMSynth().toMaster();
+const pitchInterpolater = new Tone.CtrlInterpolate([1200, 2500]);
+const volumeInterpolater = new Tone.CtrlInterpolate([5, -20]);
 
 export const Task4 = () => {
   const history = useHistory();
   const musicRef = useRef(null);
-  const box1Ref = useRef(null);
   const container = useRef(null);
   const [hidden, setHidden] = useState(false);
   const [pointerNote, setPointerNote] = useState("1");
@@ -101,6 +94,12 @@ export const Task4 = () => {
   const [show, setShow] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const [playing, setPlaying] = useState(false);
+  const [draw, redraw] = useState(0);
+
+  const [clicked, setClicked] = useState(false);
+  const [top, setTop] = useState(false);
+  const [left, setLeft] = useState(false);
+  const [play, setPlay] = useState(false);
 
   useEffect(() => {
     // TODO FINAL
@@ -114,24 +113,6 @@ export const Task4 = () => {
     });
   }, [history, pointerNote]);
 
-  const onGotPointerCapture = useCallback(
-    (e) => {
-      let elements = document.elementsFromPoint(e.clientX, e.clientY);
-      let note = elements.find((item) => item.dataset.note);
-      if (note) {
-        if (note.dataset.note - pointerNote === 1) {
-          setPointerNote(note.dataset.note);
-        }
-
-        if (note.dataset.note - pointerNote > 1) {
-          setKeyDrag(keyDrag + "_1");
-          setPointerNote("1");
-        }
-      }
-    },
-    [keyDrag, pointerNote]
-  );
-
   const touchstart = (event) => {
     Tone.context.resume();
     if (container.current) {
@@ -141,13 +122,14 @@ export const Task4 = () => {
     }
   };
 
-  const touchend = (event) => {
+  const touchend = useCallback(() => {
     synth.triggerRelease();
     setKeyDrag(keyDrag + "_1");
     setPointerNote("1");
-  };
+  }, [keyDrag]);
 
   const touchmove = (event) => {
+    onGotPointerCapture(event);
     if (container.current) {
       let t0 = event?.touches?.item(0) ?? event;
       let freq = freqFromX(t0.clientX, container.current.clientWidth);
@@ -155,92 +137,210 @@ export const Task4 = () => {
     }
   };
 
+  useEffect(() => {
+    return () => {
+      synth.dispose();
+      pitchInterpolater.dispose();
+      volumeInterpolater.dispose();
+    };
+  }, []);
+
+  const onGotPointerCapture = useCallback(
+    (event) => {
+      let e = event?.touches?.item(0) ?? event;
+
+      let elements = document.elementsFromPoint(e.clientX, e.clientY);
+      let note = elements.find((item) => item.dataset.note);
+      if (note) {
+        if (note.dataset.note - pointerNote === 1) {
+          console.log("@@___");
+          setPointerNote(note.dataset.note);
+        }
+
+        if (note.dataset.note - pointerNote > 1) {
+          console.log("@@");
+          setPointerNote("1");
+          touchend(event);
+          return false;
+        }
+      }
+    },
+    [pointerNote, touchend]
+  );
+
+  const handleMouseOut = ({ currentTarget, relatedTarget }) => {
+    if (currentTarget && !currentTarget.contains(relatedTarget)) {
+      setClicked(false);
+      setTop(0);
+      setLeft(0);
+    }
+  };
+
+  const handleMouseDown = ({ currentTarget, relatedTarget }) => {
+    if (currentTarget && !currentTarget.contains(relatedTarget)) {
+      setClicked(true);
+    }
+  };
+
+  const handleMouseUp = ({ currentTarget, relatedTarget }) => {
+    if (currentTarget && !currentTarget.contains(relatedTarget)) {
+      setClicked(false);
+    }
+    if (pointerNote !== 5) {
+      setPointerNote("1");
+    }
+  };
+
+  const handleMouseMove = ({
+    currentTarget,
+    relatedTarget,
+    clientX,
+    clientY,
+  }) => {
+    if (clicked && currentTarget && !currentTarget.contains(relatedTarget)) {
+      const element = currentTarget;
+      setLeft((clientX - element.offsetLeft) / element.offsetWidth);
+      setTop((clientY - element.offsetTop) / element.offsetHeight);
+
+      let elements = document.elementsFromPoint(clientX, clientY);
+      let note = elements.find((item) => item.dataset.note);
+      if (note) {
+        if (note.dataset.note - pointerNote === 1) {
+          setPointerNote(note.dataset.note);
+        }
+
+        if (note.dataset.note - pointerNote > 1) {
+          setPointerNote("1");
+        }
+      }
+    }
+  };
+
+  const handleMoveElem = ({ target }) => {
+    if (target) {
+      if (target.dataset.note - pointerNote === 1) {
+        console.log("@@___");
+        setPointerNote(target.dataset.note);
+      }
+
+      if (target.dataset.note - pointerNote > 1) {
+        console.log("@@");
+        setPointerNote("1");
+        touchend(target);
+        return false;
+      }
+    }
+  };
+
+  useEffect(() => {
+    pitchInterpolater.index = left;
+    volumeInterpolater.index = top;
+    const pitch = pitchInterpolater.value;
+    const volume = volumeInterpolater.value;
+
+    if (play) {
+      synth.volume.value = volume;
+      synth.setNote(pitch);
+    }
+
+    if (clicked && !play) {
+      synth.triggerAttack(pitch);
+      setPlay(true);
+    }
+
+    if (play && !clicked) {
+      synth.triggerRelease();
+      setPlay(false);
+    }
+  }, [clicked, left, play, top]);
+
   return (
     <Xwrapper>
-      <StyledTask onPointerMove={throttle(onGotPointerCapture, 300)}>
+      <StyledTask>
         <StyledTaskCursorCircle />
         <Container
-          ref={container}
-          onTouchMove={throttle(touchmove, 200)}
-          onTouchStart={throttle(touchstart, 200)}
-          onTouchEnd={throttle(touchend, 200)}
-          onPointerUp={throttle(touchend, 200)}
-          onMouseMove={throttle(touchmove, 200)}
+          onMouseOut={throttle(handleMouseOut, 100)}
+          onMouseDown={throttle(handleMouseDown, 100)}
+          onMouseUp={throttle(handleMouseUp, 100)}
+          onMouseMove={throttle(handleMouseMove, 100)}
+          onDragStart={(e) => e.preventDefault()}
         >
-          <StyledCirclesWrapper loaded={loaded}>
+          <StyledCirclesWrapper loaded={loaded} ref={container}>
             <StyledCircles1 />
             <StyledCircles2 />
             <StyledCircles3 />
             <StyledCircles4 />
             <StyledCircles5 />
-            <WrapperPlay1
-              onMouseDown={throttle(touchstart, 200)}
-              onMouseUp={throttle(touchend, 200)}
-            >
-              <DraggableBox
-                ref={box1Ref}
-                key={keyDrag}
-                id="drag"
-                item={StyledTaskPlayIconImg1}
-              ></DraggableBox>
-            </WrapperPlay1>
             <ToneButton
               note={1}
               pointerNote={pointerNote}
               setPointerNote={setPointerNote}
+              onMouseMove={handleMoveElem}
             >
-              <WrapperPlay1>
-                <StyledTaskPlayIconImg1 id="elem1" data-note={1} />
+              <WrapperPlay1 draggable={false}>
+                <StyledTaskPlayIconImg1
+                  draggable={false}
+                  id="elem1"
+                  data-note={1}
+                />
               </WrapperPlay1>
             </ToneButton>
             <ToneButton
               note={2}
               pointerNote={pointerNote}
               setPointerNote={setPointerNote}
+              onMouseMove={handleMoveElem}
             >
-              <WrapperPlay2>
-                <StyledTaskPlayIconImg2 id="elem2" data-note={2} />
+              <WrapperPlay2 draggable={false}>
+                <StyledTaskPlayIconImg2
+                  draggable={false}
+                  id="elem2"
+                  data-note={2}
+                />
               </WrapperPlay2>
             </ToneButton>
             <ToneButton
               note={3}
               pointerNote={pointerNote}
               setPointerNote={setPointerNote}
+              onMouseMove={handleMoveElem}
             >
-              <WrapperPlay3>
-                <StyledTaskPlayIconImg3 id="elem3" data-note={3} />
+              <WrapperPlay3 draggable={false}>
+                <StyledTaskPlayIconImg3
+                  draggable={false}
+                  id="elem3"
+                  data-note={3}
+                />
               </WrapperPlay3>
             </ToneButton>
             <ToneButton
               note={4}
               pointerNote={pointerNote}
               setPointerNote={setPointerNote}
+              onMouseMove={handleMoveElem}
             >
-              <WrapperPlay4>
-                <StyledTaskPlayIconImg4 id="elem4" data-note={4} />
+              <WrapperPlay4 draggable={false}>
+                <StyledTaskPlayIconImg4
+                  draggable={false}
+                  id="elem4"
+                  data-note={4}
+                />
               </WrapperPlay4>
             </ToneButton>
             <ToneButton
               note={5}
               pointerNote={pointerNote}
               setPointerNote={setPointerNote}
+              onMouseMove={handleMoveElem}
             >
-              <WrapperPlay5>
-                <StyledTaskPlayIconImg5 id="elem5" data-note={5} />
+              <WrapperPlay5 draggable={false}>
+                <StyledTaskPlayIconImg5
+                  draggable={false}
+                  id="elem5"
+                  data-note={5}
+                />
               </WrapperPlay5>
             </ToneButton>
-
-            {pointerNote !== "5" && (
-              <Xarrow
-                start={`elem${pointerNote}`}
-                end={"drag"}
-                strokeWidth="2"
-                color="#fff"
-                showHead={false}
-                key={keyDrag}
-                path="straight"
-              />
-            )}
             {pointerNote >= "2" && (
               <Xarrow
                 start="elem1"
@@ -249,6 +349,7 @@ export const Task4 = () => {
                 color="#fff"
                 showHead={false}
                 path="straight"
+                draggable={false}
               />
             )}
             {pointerNote >= "3" && (
@@ -259,6 +360,7 @@ export const Task4 = () => {
                 color="#fff"
                 showHead={false}
                 path="straight"
+                draggable={false}
               />
             )}
             {pointerNote >= "4" && (
@@ -269,6 +371,7 @@ export const Task4 = () => {
                 color="#fff"
                 showHead={false}
                 path="straight"
+                draggable={false}
               />
             )}
             {pointerNote >= "5" && (
@@ -279,6 +382,7 @@ export const Task4 = () => {
                 color="#fff"
                 showHead={false}
                 path="straight"
+                draggable={false}
               />
             )}
             <div id="theremin-container">
